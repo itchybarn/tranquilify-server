7fe0d6fd82d77a682b4e56a53dd2479f6333e87a
itchybarn <itchybarn2@gmail.com>
2026-05-26 14:43:12 -0400

    Finished database template stuff (i believe?). The /health endpoint now displays the database status

---
+++ app/db/session.py
@@ -0,0 +1,29 @@
+from sqlalchemy.ext.asyncio.session import AsyncSession
+
+
+from collections.abc import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from app.core.config import settings
+
+# the engine is the connection pool from which we'll form connections
+# echo being False keeps SQL out of the logs. should be True when debugging!
+engine = create_async_engine(settings.DATABASE_URL, echo=False)
+
+# a session tracks a connection (from the pool), open transation on that connection, and a small cache that tracks ORM objects
+# if a connection exists, it'll use that. if not, it'll create another. connections stay
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=False, # since we are usingt asyncio
+    class_=AsyncSession
+)
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    # async with ensures that the connection is given back to the pool once it's finished
+    async with AsyncSessionLocal() as session:
+        yield session # waits here until the session is done being used
--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db.session import get_async_session
 
 app = FastAPI(title="Tranquilify Server", version="0.1.0")
 
 @app.get("/health")
-async def health() -> dict[str, str]:
-    return {"status": "ok"}
+async def health(session: AsyncSession = Depends(get_async_session)) -> dict[str, str]:
+    try:
+        await session.execute(text("SELECT 1"))
+        db_status = "ok"
+    except Exception:
+        db_status = "error"
+
+    return {"status": "ok", "db": db_status}
